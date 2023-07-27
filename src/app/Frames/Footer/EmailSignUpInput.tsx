07a291cee2b3747df74980fc1e0ec2f286e204ab
Re9iNee <attarzadeh76@gmail.com>
2023-07-27 12:26:15 +0330

    footer  input style

--- src/app/Frames/Footer/EmailSignUpInput.tsx
+++ src/app/Frames/Footer/EmailSignUpInput.tsx
@@ -12,13 +12,11 @@ const EmailSignUpInput = () => {
             <input
                 type='email'
                 name='subscribe-email'
-                className='block w-full text-sm p-4 text-gray-900 rounded-3xl bg-gray-100 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none
-                custom-box-shadow 
-                sm:text-sm
-                sm:leading-6'
+                className='block w-full text-sm p-4 shadow-2xl shadow-slate-200 border border-slate-200 text-gray-900 rounded-3xl bg-neutral-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none 
+                sm:text-sm sm:leading-6'
                 placeholder='example@gmail.com'
             />
-            <div className='absolute inset-y-0 right-0 flex items-center'>
+            <div className='absolute inset-y-0 right-2 flex items-center'>
                 <button className='custom-gradient text-white text-sm font-semibold px-4 py-3 rounded-3xl'>
                     Sign-up for Updates
                 </button>
