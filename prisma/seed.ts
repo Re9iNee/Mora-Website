151338eeaf6d849666c0ede6bb91d6d33ac9cb99
Re9iNee <attarzadeh76@gmail.com>
2023-08-08 22:51:54 +0330

    reading admin credentials from .env file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,33 @@
 import { PrismaClient } from "@prisma/client";
 import { hash } from "bcryptjs";
 
+import * as dotenv from "dotenv";
+dotenv.config(); // Load the environment variables
+
 const prisma = new PrismaClient();
 
-async function main() {
-    const password = await hash("1234", 12);
-    const user = await prisma.user.upsert({
-        where: { email: "attarzadeh76@gmail.com" },
+async function createAdmin() {
+    const credential = process.env.ADMINS_CREDENTIAL;
+    if (!credential)
+        throw new Error("Please Add ADMINS_CREDENTIAL in .env file");
+
+    const [email, password] = credential.split(":");
+    const hashedPassword = await hash(password, 12);
+
+    const newUser = await prisma.user.upsert({
+        where: { email },
         update: {},
         create: {
-            email: "attarzadeh76@gmail.com",
-            name: "test",
-            password,
+            email: email,
+            name: "Admin",
+            password: hashedPassword,
         },
     });
-    console.log({ user });
+
+    console.log({ newUser });
 }
-main()
+
+createAdmin()
     .then(() => prisma.$disconnect())
     .catch(async (e) => {
         console.error(e);
