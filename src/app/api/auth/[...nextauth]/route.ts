e69a9f6138762cd429d2e085c191f9a3fba445b7
Re9iNee <attarzadeh76@gmail.com>
2023-08-10 23:43:39 +0330

    fix-build error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import { compare } from "bcryptjs";
 import NextAuth, { AuthOptions, User } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
     session: { strategy: "jwt" },
     providers: [
         CredentialsProvider({
