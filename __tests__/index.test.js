2eeeaba17a7279465ac5cdd3a7c85b398ea11a5c
Re9iNee <attarzadeh76@gmail.com>
2023-09-07 14:52:51 +0330

    tests-init

---
+++ __tests__/index.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Footer from "../src/app/Frames/Footer/index";
+import "@testing-library/jest-dom";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Footer />);
+
+    const heading = screen.getByRole("heading", {
+      name: /MORA/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
