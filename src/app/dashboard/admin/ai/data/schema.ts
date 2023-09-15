442f29ff087713178000fe5729c2978be40416a3
Re9iNee <attarzadeh76@gmail.com>
2023-09-15 11:35:13 +0330

    changing schema file to format slug

--- src/app/dashboard/admin/ai/data/schema.ts
+++ src/app/dashboard/admin/ai/data/schema.ts
@@ -2,9 +2,16 @@ import { ComplexityLevel } from "@prisma/client";
 import { z } from "zod";
 
 export const AiSchema = z.object({
-  slug: z.string(),
+  slug: z
+    .string()
+    .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+      message: "Must not have an special character",
+    })
+    .min(3)
+    .max(50)
+    .toLowerCase(),
   body: z.string(),
-  title: z.string(),
+  title: z.string().min(3).max(50),
   version: z.string().optional(),
   AI_release_date: z.date().nullish(),
   usage_link: z.string().url().optional(),
