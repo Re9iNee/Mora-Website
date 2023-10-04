4686b5b2e742557584fa7ba99c27d01a1b60067e
Re9iNee <attarzadeh76@gmail.com>
2023-10-04 22:00:39 +0330

    showing toast message after creating ai

--- src/app/dashboard/admin/ai/new/page.tsx
+++ src/app/dashboard/admin/ai/new/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { toast } from "@/components/ui/use-toast";
-import { AI } from "../data/schema";
+import { AI, AiSchema } from "../data/schema";
 import AiForm from "../form";
 import Link from "next/link";
+import { z } from "zod";
 
 async function createAi(data: AI) {
   const response = await fetch("/api/ai", {
@@ -16,19 +17,21 @@ async function createAi(data: AI) {
 
 const CreateAi = () => {
   function onSubmit(data: AI) {
-    createAi(data);
-
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <div>
-          <Link href={`./edit/${data.slug}`}>Edit Page</Link>
-          <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
-            <code className='text-white'>{JSON.stringify(data, null, 2)}</code>
-          </pre>
-        </div>
-      ),
-    });
+    createAi(data).then(() =>
+      toast({
+        title: "You submitted the following values:",
+        description: (
+          <div>
+            <Link href={`./edit/${data.slug}`}>Edit Page</Link>
+            <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
+              <code className='text-white'>
+                {JSON.stringify(data, null, 2)}
+              </code>
+            </pre>
+          </div>
+        ),
+      })
+    );
   }
 
   return <AiForm onSubmit={onSubmit} />;
