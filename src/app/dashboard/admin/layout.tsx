d83d665e877c10e1b0481dde6248b3c4e9dfffea
Re9iNee <attarzadeh76@gmail.com>
2023-10-04 22:58:04 +0330

    Fixing theming problem

--- src/app/dashboard/admin/layout.tsx
+++ src/app/dashboard/admin/layout.tsx
@@ -31,12 +31,12 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <div className='hidden space-y-6 p-10 pb-16 md:block h-screen'>
-      <div className='space-y-0.5'>
+      <header className='space-y-0.5'>
         <h2 className='text-2xl font-bold tracking-tight'>Admin Dashboard</h2>
         <p className='text-muted-foreground'>
           Manage account settings and website preferences.
         </p>
-      </div>
+      </header>
       <Separator className='my-6' />
       <div className='flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0'>
         <aside className='-mx-4 lg:w-1/5'>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,49 @@
-import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 import AnuratiRegular from "next/font/local";
+import "./globals.css";
 import { NextAuthProvider } from "./providers";
 
 const logoFont = AnuratiRegular({
-    src: "./fonts/Anurati-Regular.otf",
-    variable: "--font-anurati",
+  src: "./fonts/Anurati-Regular.otf",
+  variable: "--font-anurati",
 });
 
 const notoSans = Noto_Sans({
-    subsets: ["latin"],
-    weight: ["200", "400", "700", "800", "900"],
+  subsets: ["latin"],
+  weight: ["200", "400", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
-    title: "MORA - The AI Wiki Website",
-    description: "Mora is a Wiki for AIs",
-    themeColor: "#ffffff",
+  title: "MORA - The AI Wiki Website",
+  description: "Mora is a Wiki for AIs",
+  themeColor: "#ffffff",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang='en'>
-            <body
-                className={`${notoSans.className} ${logoFont.variable} 
+  return (
+    <html lang='en'>
+      <body
+        className={`${notoSans.className} ${logoFont.variable} 
                 bg-white
                 dark:bg-slate-900 dark:text-slate-50`}
-            >
-                <NextAuthProvider>{children}</NextAuthProvider>
-            </body>
-        </html>
-    );
+      >
+        <NextAuthProvider>
+          <ThemeProvider
+            enableSystem
+            attribute='class'
+            defaultTheme='system'
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </NextAuthProvider>
+      </body>
+    </html>
+  );
 }
