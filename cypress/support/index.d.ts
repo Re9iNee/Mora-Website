0aa3f38846df39e1d7d22b38e36a51c8bae16d2a
Re9iNee <attarzadeh76@gmail.com>
2023-10-11 14:59:56 +0330

    remove comment

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -2,10 +2,6 @@
 
 declare namespace Cypress {
   interface Chainable<Subject = any> {
-    /**
-     * Custom command to ... add your description here
-     * @example cy.login()
-     */
     login(email: string, password: string): Chainable<any>;
   }
 }
