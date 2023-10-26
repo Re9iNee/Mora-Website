c7a21002f4c1440f6beca7dd2735981e39e3022e
Re9iNee <attarzadeh76@gmail.com>
2023-10-26 22:22:41 +0330

    adding form label to tags select

--- src/app/dashboard/admin/tag/select.tsx
+++ src/app/dashboard/admin/tag/select.tsx
@@ -4,6 +4,7 @@ import { getTagsByName } from "@/services/tag.service";
 import { ControllerRenderProps } from "react-hook-form";
 import AsyncSelect from "react-select/async";
 import { Tag } from "./schema";
+import { FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 
 interface Props {
   field: ControllerRenderProps<any, "tags">;
@@ -15,26 +16,30 @@ function TagSelect({ field, ...props }: Props) {
     });
 
   return (
-    <AsyncSelect
-      cacheOptions
-      loadOptions={promiseOptions}
-      defaultOptions
-      menuPlacement='auto'
-      // set option labels color red
-      styles={{
-        // TODO: Make it more like shadcn
-        option: (provided, state) => ({
-          ...provided,
-          color: state.isSelected ? "red" : "blue",
-        }),
-      }}
-      isMulti={true}
-      getOptionValue={(tag) => tag.id}
-      getOptionLabel={(tag) => tag.name}
-      defaultValue={field.value}
-      {...field}
-      {...props}
-    />
+    <FormItem>
+      <FormLabel>Tags</FormLabel>
+      <AsyncSelect
+        cacheOptions
+        loadOptions={promiseOptions}
+        defaultOptions
+        menuPlacement='auto'
+        // set option labels color red
+        styles={{
+          // TODO: Make it more like shadcn
+          option: (provided, state) => ({
+            ...provided,
+            color: state.isSelected ? "red" : "blue",
+          }),
+        }}
+        isMulti={true}
+        getOptionValue={(tag) => tag.id}
+        getOptionLabel={(tag) => tag.name}
+        defaultValue={field.value}
+        {...field}
+        {...props}
+      />
+      <FormMessage />
+    </FormItem>
   );
 }
 
