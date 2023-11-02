55ea12f1549bb8ddf4bbc272fdb4920b22805f33
Re9iNee <attarzadeh76@gmail.com>
2023-11-03 01:02:06 +0330

    adding schema field to ai

--- src/app/dashboard/admin/ai/data/schema.ts
+++ src/app/dashboard/admin/ai/data/schema.ts
@@ -14,12 +14,14 @@ export const AiSchema = z.object({
   body: z.string(),
   tags: z.array(TagSchema),
   version: z.string().optional(),
+  logo_alt: z.string().optional(),
   title: z.string().min(3).max(50),
   AI_release_date: z.date().nullish(),
   usage_link: z.string().url().optional(),
   origin_website: z.string().url().optional(),
   video: z.object({ name: z.string(), id: z.string() }).optional(),
   complexity_level: z.nativeEnum(ComplexityLevel).default("NORMAL"),
+  google_query_text: z.string().optional(),
 });
 
 // DIFFERENCES {
