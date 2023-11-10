a3dccef6614bc818cd2582fd61388e006187448c
Re9iNee <attarzadeh76@gmail.com>
2023-11-10 13:42:32 +0330

    Footer darkmode

--- src/app/Frames/Footer/index.tsx
+++ src/app/Frames/Footer/index.tsx
@@ -5,7 +5,7 @@ import EmailSignUpInput from "./EmailSignUpInput";
 const Footer = () => {
   return (
     <footer
-      className='max-w-screen-xl px-4 py-6 flex flex-col gap-6 text-left  bg-neutral-100 rounded-t-largest footer-shadow-mobile dark:bg-gray-800
+      className='max-w-screen-xl px-4 py-6 flex flex-col gap-6 text-left  bg-neutral-100 rounded-t-largest footer-shadow-mobile dark:bg-neutral-950
       md:px-8 md:gap-8
       lg:px-16 lg:gap-10 lg:pt-12
       xl:mx-auto xl:px-20
@@ -34,10 +34,10 @@ const Footer = () => {
           </h4>
         </section>
         <section className='lg:w-96 lg:h-28'>
-          <h3 className='font-bold text-neutral-800 text-base'>
+          <h3 className='font-bold text-neutral-800 text-base dark:text-neutral-200'>
             What is Mora?
           </h3>
-          <p className='text-neutral-500 text-sm leading-tight'>
+          <p className='text-neutral-500 text-sm leading-tight dark:text-neutral-400'>
             Mora is a library of AI tools and courses on how to use them to
             their fullest and like a master. Our mission is to help you take the
             most out of upcoming AI tools.
@@ -50,12 +50,14 @@ const Footer = () => {
         md:flex-row md:justify-between md:gap-24'
       >
         <section className='flex flex-col gap-2'>
-          <h3 className='text-neutral-800 font-bold text-lg'>Find us on</h3>
+          <h3 className='text-neutral-800 font-bold text-lg dark:text-neutral-200'>
+            Find us on
+          </h3>
           {/* footer links */}
           <section className='flex gap-2'>
-            <FiLinkedin className='text-sky-700 w-8 h-8' />
-            <FiInstagram className='text-pink-600 w-8 h-8' />
-            <FaXTwitter className='w-8 h-8' />
+            <FiLinkedin className='text-sky-700 w-8 h-8 dark:text-sky-500' />
+            <FiInstagram className='text-pink-600 w-8 h-8 dark:text-pink-400' />
+            <FaXTwitter className='w-8 h-8 dark:text-neutral-50' />
           </section>
         </section>
 
@@ -64,7 +66,7 @@ const Footer = () => {
           lg:w-96 lg:h-28
           '
         >
-          <h3 className='text-neutral-800 font-bold'>
+          <h3 className='text-neutral-800 font-bold dark:text-neutral-200'>
             Subscribe to get updates
           </h3>
           <button
@@ -78,7 +80,7 @@ const Footer = () => {
         </section>
       </section>
 
-      <p className='text-center text-neutral-500 text-sm font-normal leading-tight'>
+      <p className='text-center text-neutral-500 text-sm font-normal leading-tight dark:text-neutral-400'>
         ©️Copyright. All rights reserved.
       </p>
     </footer>
