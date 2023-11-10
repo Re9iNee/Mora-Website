583eb36550a03c6f23709771f31614c3e760aea0
Re9iNee <attarzadeh76@gmail.com>
2023-11-10 12:37:42 +0330

    Footer XS Complete

--- src/app/Frames/Footer/index.tsx
+++ src/app/Frames/Footer/index.tsx
@@ -1,45 +1,76 @@
+import { FiInstagram, FiLinkedin } from "react-icons/fi";
+import { FaXTwitter } from "react-icons/fa6";
 import EmailSignUpInput from "./EmailSignUpInput";
 
 const Footer = () => {
-    return (
-        <footer
-            className='max-w-screen-xl text-center xl:mx-auto mt-16 p-10 pt-6 bg-white rounded-t-largest footer-shadow-mobile dark:bg-gray-800
-            sm:grid sm:grid-cols-12
+  return (
+    <footer
+      className='max-w-screen-xl px-4 py-6 flex flex-col gap-6 text-left  bg-neutral-100 rounded-t-largest footer-shadow-mobile dark:bg-gray-800
+      sm:grid sm:grid-cols-12
+      xl:mx-auto
+      '
+    >
+      {/* Brand info */}
+      <section className='text-left flex flex-col gap-4 sm:col-span-12'>
+        <section className='flex flex-col gap-2'>
+          <h2
+            className='gradient-logo font-anurati text-4xl font-black
+            sm:text-5xl sm:drop-shadow-lg
             '
-        >
-            <section className='text-center sm:col-span-12'>
-                <h2
-                    className='gradient-logo font-anurati text-4xl font-black
-                    sm:text-5xl sm:drop-shadow-lg
-                    '
-                >
-                    MORA
-                </h2>
-                <h4
-                    className='gradient-logo font-extralight tracking-widest
-                    sm:text-xl sm:drop-shadow-lg
-                    '
-                >
-                    Do More with Less Work
-                </h4>
-            </section>
+          >
+            MORA
+          </h2>
+          <h4
+            className='text-sm gradient-logo font-extralight tracking-widest 
+            sm:text-xl sm:drop-shadow-lg
+            '
+          >
+            Do More with Less Work
+          </h4>
+        </section>
+        <section>
+          <h3 className='font-bold text-neutral-800 text-base'>
+            What is Mora?
+          </h3>
+          <p className='text-neutral-500 text-sm leading-tight'>
+            Mora is a library of AI tools and courses on how to use them to
+            their fullest and like a master. Our mission is to help you take the
+            most out of upcoming AI tools.
+          </p>
+        </section>
+      </section>
 
-            <button
-                className='mt-8 custom-gradient text-white text-sm font-semibold px-4 py-3 rounded-3xl
-                sm:hidden
-                '
-            >
-                Sign-up for Updates
-            </button>
+      <section className='flex flex-col gap-4'>
+        <section className='flex flex-col gap-2'>
+          <h3 className='text-neutral-800 font-bold text-lg'>Find us on</h3>
+          {/* footer links */}
+          <section className='flex gap-2'>
+            <FiLinkedin className='text-sky-700 w-8 h-8' />
+            <FiInstagram className='text-pink-600 w-8 h-8' />
+            <FaXTwitter className='w-8 h-8' />
+          </section>
+        </section>
 
-            <EmailSignUpInput />
+        <section className='flex flex-col gap-3'>
+          <h3 className='text-neutral-800 font-bold'>
+            Subscribe to get updates
+          </h3>
+          <button
+            className='h-11 self-stretch  bg-purple-600 font-bold text-white text-sm px-10 py-3 rounded-2xl
+            sm:hidden
+            '
+          >
+            Sign-up
+          </button>
+          <EmailSignUpInput />
+        </section>
+      </section>
 
-            <p className='mt-8 text-xs font-light text-gray-500 text-center sm:col-span-12 dark:text-gray-400'>
-                By signing up to MORA, You’ll receive updates directly in your
-                inbox.
-            </p>
-        </footer>
-    );
+      <p className='text-center text-neutral-500 text-sm font-normal leading-tight'>
+        ©️Copyright. All rights reserved.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
